cce009d35243623ed84562613b1d3f738a7f13da
Olcay Uzun <o.uzuns@gmail.com>
2021-11-03 10:07:31 +0300

    create config

--- server/app.js
+++ server/app.js
@@ -1,5 +1,12 @@
 const expresss = require('express');
+const helmet = require('helmet');
+const config = require('./config');
 const app = expresss();
+
+config();
+
+app.use(expresss.json());
+app.use(helmet());
 
 app.listen(1717, () => {
     console.log('Server is running on 1717...');
